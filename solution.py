36d40d70fe06aa64661d5602f4b5ed1028996e0a
Pablo Trinidad <p@pablotrinidad.me>
2020-06-22 21:11:39 -0500

    Added problem description

---
+++ solution.py
@@ -0,0 +1,10 @@
+"""Solution by <USERNAME>."""
+
+from typing import List
+
+
+class Solution:
+    """Solution to the partition problem."""
+
+    def partition(self, A: List[int]):  # pylint: disable=C0103
+        """Partition the array using the first element as pivot."""
